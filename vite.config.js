bb5ef1966af5d4aec4d33c334d4e212936aecb4d
Roachkinq21 <thedoodlekidd@gmail.com>
2025-04-20 15:10:01 -0400

    updte work and vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-fimport { resolve } from 'path'
+import { resolve } from 'path'
 
-export default{
+export default {
   base: '/https://github.com/Roachkinq21/HOM_Website.git',
   root: resolve(__dirname, 'src'),
   build: {
